64cec83d6232cebde38a435ed9bf281fbea99435
ayano-w <ayasaku.t5@gmail.com>
2021-10-30 13:55:40 +0000

    [Add]add favorite fanctions before creating a View.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/about' => 'homes#about'
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
 end
